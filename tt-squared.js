7ed00dc7e0a6223f6b3aaaa39eebc9514d015ebd
Corey Ballou <ballouc@gmail.com>
2011-11-18 09:41:20 -0500

    Adding handling for searching iTunes.

--- tt-squared.js
+++ tt-squared.js
@@ -753,49 +753,6 @@
 	}
 
 	/**
-	 * Given a set of itunes search results, display them.
-	 * http://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html#lookup
-	 */
-	function handleItunesResults(arg) {
-		var results = arg.results;
-		_log(results);
-		var html = '';
-		for (var i = 0; i < results.length; i++) {
-			/*
-			var item = results[i];
-			var obj = {
-				source: 0,
-				track_id: item.trackId,
-				track_name: item.trackCensoredName,
-				track_url: item.trackViewUrl,
-				artist_name: item.artistName,
-				artist_url: item.artistViewUrl,
-				collection_name: item.collectionCensoredName,
-				collection_url: item.collectionViewUrl,
-				genre: item.primaryGenreName
-			};
-			results[i] = obj;
-
-			html += '<div class="songs-search-result">';
-			html += '<span class="label">Track:</span>{0}&nbsp;&nbsp;'.replace("{0}", obj.track_name);
-			html += '<a href="{0}" target="_blank">Preview</a>&nbsp;&nbsp;'.replace("{0}", item.previewUrl);
-			html += '<a href="{0}" target="_blank">Full Song</a>&nbsp;&nbsp;'.replace("{0}", obj.track_url);
-			html += '<span class="label">Track Price:</span>{0} {1}<br />'.replace("{0}", item.trackPrice).replace("{1}", item.currency);
-			html += '<span class="label">Artist:</span><a href="{0}" target="_blank">{1}</a><br />'.replace("{0}", obj.artist_url).replace("{1}", obj.artist_name);
-			html += '<span class="label">Collection:</span><a href="{0}" target="_blank">{1}</a><br />'.replace("{0}", obj.collection_url).replace("{1}", obj.collection_name);
-			html += '<span class="label">Collection Price:</span>{0} {1}<br />'.replace("{0}", item.collectionPrice).replace("{1}", item.currency);
-			html += '<span class="label">Primary Genre:</span>{0}<br />'.replace("{0}", obj.genre);
-			html += '</div>';
-			*/
-		}
-
-		//$('#itunes-results').html(html);
-
-		// clean up the JS from HEAD
-		$('head').find('script[src^="http://ax.itunes.apple.com"]').remove();
-	}
-
-	/**
 	 * Urlencode a flat JSON object.
 	 */
 	function urlencode(params) {
@@ -916,3 +873,47 @@
 	}
 
 })();
+
+/**
+ * Given a set of itunes search results, display them.
+ * http://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html#lookup
+ http://www.rahulsingla.com/blog/2011/08/itunes-performing-itunes-store-search-in-javascript
+ */
+function handleItunesResults(arg) {
+	var results = arg.results;
+	_log(results);
+	var html = '';
+	for (var i = 0; i < results.length; i++) {
+		/*
+		var item = results[i];
+		var obj = {
+			source: 0,
+			track_id: item.trackId,
+			track_name: item.trackCensoredName,
+			track_url: item.trackViewUrl,
+			artist_name: item.artistName,
+			artist_url: item.artistViewUrl,
+			collection_name: item.collectionCensoredName,
+			collection_url: item.collectionViewUrl,
+			genre: item.primaryGenreName
+		};
+		results[i] = obj;
+
+		html += '<div class="songs-search-result">';
+		html += '<span class="label">Track:</span>{0}&nbsp;&nbsp;'.replace("{0}", obj.track_name);
+		html += '<a href="{0}" target="_blank">Preview</a>&nbsp;&nbsp;'.replace("{0}", item.previewUrl);
+		html += '<a href="{0}" target="_blank">Full Song</a>&nbsp;&nbsp;'.replace("{0}", obj.track_url);
+		html += '<span class="label">Track Price:</span>{0} {1}<br />'.replace("{0}", item.trackPrice).replace("{1}", item.currency);
+		html += '<span class="label">Artist:</span><a href="{0}" target="_blank">{1}</a><br />'.replace("{0}", obj.artist_url).replace("{1}", obj.artist_name);
+		html += '<span class="label">Collection:</span><a href="{0}" target="_blank">{1}</a><br />'.replace("{0}", obj.collection_url).replace("{1}", obj.collection_name);
+		html += '<span class="label">Collection Price:</span>{0} {1}<br />'.replace("{0}", item.collectionPrice).replace("{1}", item.currency);
+		html += '<span class="label">Primary Genre:</span>{0}<br />'.replace("{0}", obj.genre);
+		html += '</div>';
+		*/
+	}
+
+	//$('#itunes-results').html(html);
+
+	// clean up the JS from HEAD
+	$('head').find('script[src^="http://ax.itunes.apple.com"]').remove();
+}
