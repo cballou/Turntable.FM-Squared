d6d74f2e6d01bac476c6ec229f1e04b83bcbeafb
Corey Ballou <ballouc@gmail.com>
2011-11-28 08:32:49 -0500

    Similar tracks handler.

--- tt-squared.js
+++ tt-squared.js
@@ -1000,11 +1000,17 @@ function _log(msg) {
  * http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=cher&track=believe&api_key=d1b14c712954973f098a226d80d6b5c2
  */
 function getSimilarTracks(artist, song, album) {
-	var url = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&autocorrect=1&artist=' + artist + '&track=' + song + '&api_key=d1b14c712954973f098a226d80d6b5c2&format=json&callback=?';
+	var url = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&autocorrect=1&artist=' + encodeURIComponent(artist) + '&track=' + encodeURIComponent(song) + '&api_key=d1b14c712954973f098a226d80d6b5c2&format=json&callback=?';
 	$.getJSON(url, function(data) {
-		_log(data);
 		var html = '';
-		$.each(similartracks.track, function(i, item) {
+		if (!data || !data.similartracks || !data.similartracks.track) {
+			return;
+		}
+
+		// log similar tracks until we're happy
+		_log(data.similartracks);
+
+		$.each(data.similartracks.track, function(i, item) {
 
 			// name
 			// playcount
@@ -1023,18 +1029,22 @@ function getSimilarTracks(artist, song, album) {
 			html += '<p><span style="float:left;display:inline;width:100px;">Track:</span>' + item.name + '</p>';
 			if (item.mbid.length) {
 				html += '<p><a href="#" style="display:block">Preview &amp; Buy Track</a>';
+				// get buy links and change them
+				var buyUrl = 'http://ws.audioscrobbler.com/2.0/?method=track.getbuylinks&artist=' + encodeURIComponent(artist) + '&track=' + encodeURIComponent(song) + '&api_key=d1b14c712954973f098a226d80d6b5c2&format=json&callback=?';
+				$.getJSON(buyUrl, function(data) {
+					_log('buy url data');
+					_log(data);
+				});
 			}
-			if (item.artist.mbid.length) {
-				html += '<p><a href="#" style="display:block">View Artist Details</a>';
-			}
+			//if (item.artist.mbid.length) {
+			//	html += '<p><a href="#" style="display:block">View Artist Details</a>';
+			//}
 			html += '</li>';
 		});
 
 		if (html.length) {
 			// append html
 			$('#similarTracks').find('ul').html(html);
-			$('#similarTracks').show();
-		} else {
 			$('#similarTracks').show();
 		}
 	});
