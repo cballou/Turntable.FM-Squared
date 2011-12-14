0482d82f82576be94ed3eff39f6c30bc52ceace8
Corey Ballou <ballouc@gmail.com>
2011-12-14 10:00:45 -0500

    Updating chrome extension to inject css instead of remotely injecting from github.

--- chrome-ext/tt-squared.js
+++ chrome-ext/tt-squared.js
@@ -1,10 +1,12 @@
 function ttSquared() {
 	// inject stylesheet
+	/*
 	var s = document.createElement('link');
 	s.setAttribute('rel', 'stylesheet');
 	s.setAttribute('type', 'text/css');
 	s.setAttribute('href', 'https://raw.github.com/cballou/Turntable.FM-Anti-Idle-Autoresponder/master/tt-squared.css');
 	document.head.appendChild(s);
+	*/
 
 	// inject javascript
 	var s = document.createElement('script');
