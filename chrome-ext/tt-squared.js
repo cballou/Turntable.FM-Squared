bccd445a87271db2aa6359c8e97533f0625c32c2
Corey Ballou <ballouc@gmail.com>
2011-12-14 09:51:37 -0500

    Adding css injection to chrome ext.

--- chrome-ext/tt-squared.js
+++ chrome-ext/tt-squared.js
@@ -1,4 +1,12 @@
 function ttSquared() {
+	// inject stylesheet
+	var s = document.createElement('link');
+	s.setAttribute('rel', 'stylesheet');
+	s.setAttribute('type', 'text/css');
+	s.setAttribute('href', 'https://raw.github.com/cballou/Turntable.FM-Anti-Idle-Autoresponder/master/tt-squared.css');
+	document.head.appendChild(s);
+
+	// inject javascript
 	var s = document.createElement('script');
 	s.setAttribute('src', 'https://raw.github.com/cballou/Turntable.FM-Anti-Idle-Autoresponder/master/tt-squared.js');
 	s.setAttribute('type', 'text/javascript');
