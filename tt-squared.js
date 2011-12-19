5eb5fceb258adb6ca377a383c86b4b41e14c83cd
Corey Ballou <ballouc@gmail.com>
2011-12-19 08:54:46 -0500

    Fixing up styling and chat positioning.

--- tt-squared.js
+++ tt-squared.js
@@ -882,6 +882,8 @@ p=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u20
 		$('#tt2_chat_box').find('.chat_container').css({
 			height: tt2_size.height - tt2_playing_size.height,
 			width: tt2_size.width - tt2_playing_size.width,
+			top: 0,
+			left: 0,
 			'overflow-x': 'none',
 			'overflow-y': 'auto'
 		});
