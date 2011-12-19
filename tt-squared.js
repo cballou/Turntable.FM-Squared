b9c4e2153d3494a332f776b768fadd58d577ce6a
Corey Ballou <ballouc@gmail.com>
2011-12-19 09:42:28 -0500

    Fixing styling.

--- tt-squared.js
+++ tt-squared.js
@@ -902,8 +902,8 @@ p=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u20
 
 		// fix chat sizing in TT2
 		$('#tt2_chat_box').find('.chat_container').css({
-			height: (tt2_size.height - tt2_playing_size.height) + 'px !important',
-			width: (tt2_size.width - tt2_playing_size.width) + 'px !important',
+			height: (tt2_size.height - tt2_playing_size.height - 20) + 'px !important',
+			width: (tt2_size.width - tt2_playing_size.width - 20) + 'px !important',
 			top: '0 !important',
 			left: '0 !important',
 			'overflow-x': 'none !important',
