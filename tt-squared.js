ca68c9c27e36b122316ff865a019b39a8a31e63c
Corey Ballou <ballouc@gmail.com>
2011-12-19 13:00:09 -0500

    Unbinding resize chat.

--- tt-squared.js
+++ tt-squared.js
@@ -906,19 +906,16 @@ p=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u20
 			height: $('#tt2_playing').innerHeight()
 		};
 
-		_log('New chat height: ' + (tt2_size.height - tt2_playing_size.height - 20));
-		_log('New chat width: ' + tt2_playing_size.width);
-
 		// fix chat sizing in TT2
 		$('#tt2_chat_box').find('.chat-container').css({
-			height: '' + (tt2_size.height - tt2_playing_size.height - 20) + 'px !important',
+			height: '' + (tt2_size.height - tt2_playing_size.height - 40) + 'px !important',
 			width: '100%',
 			top: '0 !important',
 			left: '0 !important'
 		});
 
 		$('#tt2_chat_box').find('.chat-container .messages').css({
-			height: '' + (tt2_size.height - tt2_playing_size.height - title_height - 20) + 'px !important',
+			height: '' + (tt2_size.height - tt2_playing_size.height - title_height - 40) + 'px !important',
 			'overflow-x': 'none !important',
 			'overflow-y': 'auto !important'
 		});
