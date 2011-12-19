cc3ff435a1e53821e4f3035ce6b9594be61fe723
Corey Ballou <ballouc@gmail.com>
2011-12-19 09:04:41 -0500

    Fixing up some height issues with the move.

--- tt-squared.js
+++ tt-squared.js
@@ -887,7 +887,7 @@ p=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u20
 		};
 
 		// fix chat sizing in TT2
-		$('#tt2_chat_box').find('.chat_container').cssText(
+		$('#tt2_chat_box').find('.chat_container').css('cssText',
 			'height: ' + (tt2_size.height - tt2_playing_size.height) + 'px !important; ' +
 			'width: ' + (tt2_size.width - tt2_playing_size.width) + 'px !important; ' +
 			'top: 0 !important; ' +
