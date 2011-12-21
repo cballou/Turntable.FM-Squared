63cb611c394ff66cc7268eb3b1c7e3f841e5c527
Corey Ballou <ballouc@gmail.com>
2011-12-21 10:20:14 -0500

    Updates, working on last idle times.

--- tt-squared.js
+++ tt-squared.js
@@ -1370,6 +1370,8 @@ p=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u20
 	}
 
 	function formatDate(date, levels) {
+		_log(date);
+
 		var DATE_CONVERSIONS = {'year':31536000,'month':2678400,'week':604800,'day':86400,'hour':3600,'minute':60,'second':1};
 		var curdate = new Date().getTime();
 		curdate = Math.round(curdate / 1000);
