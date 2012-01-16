b89567b14c56dd78f825aa436a51a7bb06f4f5b0
Corey Ballou <ballouc@gmail.com>
2012-01-16 08:56:09 -0500

    Fixing up Greasemonkey script.

--- chrome-ext/tt-squared.js
+++ chrome-ext/tt-squared.js
@@ -15,18 +15,9 @@
 // ==/UserScript==
 
 function ttSquared() {
-	// inject stylesheet
-	/*
-	var s = document.createElement('link');
-	s.setAttribute('rel', 'stylesheet');
-	s.setAttribute('type', 'text/css');
-	s.setAttribute('href', 'https://raw.github.com/cballou/Turntable.FM-Anti-Idle-Autoresponder/master/tt-squared.css');
-	document.head.appendChild(s);
-	*/
-
 	// inject javascript
 	var s = document.createElement('script');
-	s.setAttribute('src', 'https://raw.github.com/cballou/Turntable.FM-Anti-Idle-Autoresponder/master/tt-squared.js');
+	s.setAttribute('src', 'https://raw.github.com/cballou/Turntable.FM-Squared/master/tt-squared.js');
 	s.setAttribute('type', 'text/javascript');
 	(document.body || document.head || document.documentElement).appendChild(s);
 }
--- greasemonkey/tt-squared.js
+++ greasemonkey/tt-squared.js
@@ -1,17 +1,32 @@
 // ==UserScript==
-// @match http://*/*
-// @match http://*.google.com/*
-// @match http://www.google.com/*
+// @name TT.FM Squared
+// @description A set of tools including an improved chat UI, song analytics, auto-upvote (autobop), anti-idle, auto-respond, additional track information, and similar track listings.
+// @namespace http://userscripts.org/users/119115
+// @include http://turntable.fm/*
+// @include https://turntable.fm/*
+// @exclude http://turntable.fm/lobby
+// @exclude http://turntable.fm/jobs
+// @exclude http://turntable.fm/about
+// @exclude http://turntable.fm/terms
+// @exclude http://turntable.fm/copyright
+// @exclude http://turntable.fm/privacy
+// @exclude http://turntable.fm/static*
+// @match http://turntable.fm/*
 // ==/UserScript==
 
-function ttSQuared() {
-	/**
-	 * Inject the remote script.
-	 */
-	var s = document.createElement('script');
-	s.setAttribute('src', 'https://raw.github.com/cballou/Turntable.FM-Anti-Idle-Autoresponder/master/tt-squared.js');
-	s.setAttribute('type', 'text/javascript');
-	(document.body || document.head || document.documentElement).appendChild(s);
+function ttSquared() {
+    // inject stylesheet
+    var s = document.createElement('link');
+    s.setAttribute('rel', 'stylesheet');
+    s.setAttribute('type', 'text/css');
+    s.setAttribute('href', 'https://raw.github.com/cballou/Turntable.FM-Squared/master/tt-squared.css');
+    (document.head || document.body).appendChild(s);
+
+    // inject javascript
+    var s = document.createElement('script');
+    s.setAttribute('src', 'https://raw.github.com/cballou/Turntable.FM-Squared/master/tt-squared.js');
+    s.setAttribute('type', 'text/javascript');
+    (document.body || document.head || document.documentElement).appendChild(s);
 }
 
-location.href="javascript:(function(){" + ttSQuared + "})()";
+ttSquared();
