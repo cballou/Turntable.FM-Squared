6722e03cb96f838b7b082d751550b4fd8435b288
Corey Ballou <ballouc@gmail.com>
2012-03-20 08:33:00 -0400

    Updating greasemonkey to hopefully work with chrome extensions.

--- greasemonkey/tt-squared.user.js
+++ greasemonkey/tt-squared.user.js
@@ -1,37 +1,64 @@
+// <![CDATA[
 // ==UserScript==
-// @name TT.FM Squared
-// @description A set of tools including an improved chat UI, song analytics, auto-upvote (autobop), anti-idle, auto-respond, additional track information, and similar track listings.
-// @namespace http://userscripts.org/users/119115
-// @include http://turntable.fm/*
-// @include https://turntable.fm/*
-// @exclude http://turntable.fm/lobby
-// @exclude http://turntable.fm/jobs
-// @exclude http://turntable.fm/about
-// @exclude http://turntable.fm/terms
-// @exclude http://turntable.fm/copyright
-// @exclude http://turntable.fm/privacy
-// @exclude http://turntable.fm/static*
-// @match http://turntable.fm/*
+// @name        TT.FM Squared
+// @fullname    Turntable.FM Squared
+// @description A set of Turntable.FM tools including an improved chat UI, song analytics, auto-upvote (autobop), anti-idle, auto-respond, additional track information, and similar track listings.
+// @homepage    http://userscripts.org/scripts/show
+// @icon        http://s3.amazonaws.com/uso_ss/icon/33042/large.PNG?1245499122
+// @author      http://userscripts.org/users/441482
+// @namespace   http://userscripts.org/users/441482
+// @version     1.0.0
+// @copyright   2012+, Corey Ballou (http://coreyballou.com)
+// @license     MIT license; http://opensource.org/licenses/mit-license.php
+// @run-at      document-end
+// @include     http://turntable.fm/*
+// @include     https://turntable.fm/*
+// @exclude     http://turntable.fm/lobby
+// @exclude     http://turntable.fm/jobs
+// @exclude     http://turntable.fm/about
+// @exclude     http://turntable.fm/terms
+// @exclude     http://turntable.fm/copyright
+// @exclude     http://turntable.fm/privacy
+// @exclude     http://turntable.fm/static*
+// @match       http://turntable.fm/*
 // @resource TTFM_CSS tt-squared.css
 // ==/UserScript==
 
-function ttSquared() {
-    // inject stylesheet
-    var cssTxt = GM_getResourceText('TTFM_CSS');
-    GM_addStyle(cssTxt);
-    /*
-    var s = document.createElement('link');
-    s.setAttribute('rel', 'stylesheet');
-    s.setAttribute('type', 'text/css');
-    s.setAttribute('href', 'https://raw.github.com/cballou/Turntable.FM-Squared/master/tt-squared.css');
-    (document.head || document.body).appendChild(s);
-    //var el = (document.head || document.body).appendChild(s);
-    //el.innerHTML = '@import url("https://raw.github.com/cballou/Turntable.FM-Squared/master/tt-squared.css");';
+// USERSCRIPT METADATA
+var UserScript = {
+    name: 'TT.FM Squared',
+    id: 'ttfmsq',
+    version: '1.0.0',
+    include: [
+        'http://turntable.fm/*',
+        'https://turntable.fm/*'
+    ],
+    exclude: [
+        'http://turntable.fm/lobby',
+        'http://turntable.fm/jobs',
+        'http://turntable.fm/about',
+        'http://turntable.fm/terms',
+        'http://turntable.fm/copyright',
+        'http://turntable.fm/privacy',
+        'http://turntable.fm/static*'
+    ],
+    manifest: 'http://assets.dharmafly.com/allsizes/manifest.json',
+    codebase: 'http://userscripts.org/scripts/source/6178.user.js',
+    description: 'A set of Turntable.FM tools including an improved chat UI, song analytics, auto-upvote (autobop), anti-idle, auto-respond, additional track information, and similar track listings.',
+    copyright: '(C)2012, Corey Ballou',
+    unwrap: true
+};
 
-    var head = document.getElementsByTagName("HEAD")[0];
-    var el = head.appendChild(document.createElement('style'));
-    el.innerHTML = '@import url("//raw.github.com/cballou/Turntable.FM-Squared/master/tt-squared.css");';
-    */
+/**
+ * Where the magic happens.
+ */
+function ttSquared() {
+    var isGoogleChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+    if (!isGoogleChrome) {
+        // inject stylesheet
+        var cssTxt = GM_getResourceText('TTFM_CSS');
+        GM_addStyle(cssTxt);
+    }
 
     // inject javascript
     var s = document.createElement('script');
@@ -40,4 +67,5 @@ function ttSquared() {
     (document.body || document.head || document.documentElement).appendChild(s);
 }
 
+// initialize
 ttSquared();
