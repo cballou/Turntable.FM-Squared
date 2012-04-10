b4693707a5f755883ca09b2f437d0b51c607e6af
Corey Ballou <ballouc@gmail.com>
2012-04-10 09:49:34 -0400

    Deleting tt-squared.js

--- chrome-ext/tt-squared.js
+++
@@ -1,9 +0,0 @@
-function ttSquared() {
-	// inject javascript
-	var s = document.createElement('script');
-	s.setAttribute('src', 'https://raw.github.com/cballou/Turntable.FM-Squared/master/tt-squared.js');
-	s.setAttribute('type', 'text/javascript');
-	(document.body || document.head || document.documentElement).appendChild(s);
-}
-
-ttSquared();
